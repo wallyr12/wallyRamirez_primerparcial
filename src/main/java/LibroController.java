c6951f65f9559e4acad88c450b202038add5d1a6
wally ramirez <wramirezs4@miumg.edu.gt>
2022-08-13 11:10:47 -1000

    actualizacion para intentar arreglarlo

--- src/main/java/LibroController.java
+++ src/main/java/LibroController.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Wally Ramirez
  */
-@WebServlet(urlPatterns = {"/NewServlet"})
+@WebServlet(urlPatterns = {"/LibroController"})
 public class LibroController extends HttpServlet {
      Libro libro;
     LibrosController registroLibro;
@@ -54,7 +54,7 @@ public class LibroController extends HttpServlet {
             respuesta.println("<!DOCTYPE html>");
             respuesta.println("<html>");
             respuesta.println("<head>");
-            respuesta.println("<title>Servlet NewServlet</title>");   
+            respuesta.println("<title>Servlet LibroController</title>");   
             respuesta.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' integrity='sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N' crossorigin='anonymous'>");
             respuesta.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct' crossorigin='anonymous'></script>");
             respuesta.println("</head>");
